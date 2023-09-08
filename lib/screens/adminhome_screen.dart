0a9d57d681f7f64e1857427dc0474cc2c9287116
Lans12345 <olanalans12345@gmail.com>
2023-09-08 13:06:52 +0800

    wip admin home screen

--- lib/screens/adminhome_screen.dart
+++ lib/screens/adminhome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:communal/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
 class AdminHomeScreen extends StatelessWidget {
@@ -5,6 +6,145 @@ class AdminHomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 150,
+                  decoration: const BoxDecoration(
+                    color: Colors.green,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20, top: 20),
+                    child: TextWidget(
+                      text: 'Admin Dashboard',
+                      fontSize: 28,
+                      fontFamily: 'Bold',
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 75),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      for (int i = 0; i < 4; i++)
+                        GestureDetector(
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return Dialog(
+                                  child: SizedBox(
+                                    width: 600,
+                                    height: 600,
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Align(
+                                          alignment: Alignment.topRight,
+                                          child: IconButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            icon: const Icon(
+                                              Icons.close,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            elevation: 3,
+                            child: Container(
+                              width: 250,
+                              height: 150,
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(15.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        TextWidget(
+                                          text: 'Pending',
+                                          fontSize: 18,
+                                          fontFamily: 'Medium',
+                                          color: Colors.grey,
+                                        ),
+                                        const Icon(
+                                          Icons.pending,
+                                          color: Colors.grey,
+                                          size: 32,
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    TextWidget(
+                                      text: '24',
+                                      fontSize: 28,
+                                      fontFamily: 'Bold',
+                                      color: Colors.black,
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    TextWidget(
+                                      text: 'Tourist who are pending',
+                                      fontSize: 14,
+                                      fontFamily: 'Regular',
+                                      color: Colors.grey,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Card(
+              child: Container(
+                width: double.infinity,
+                height: 375,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
