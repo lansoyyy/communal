4101e3c7b7805ec34071c65c4d91eb6e8a397c0a
Lans12345 <olanalans12345@gmail.com>
2023-09-20 15:57:15 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:communal/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          authDomain: 'communal-967dd.firebaseapp.com',
+          apiKey: "AIzaSyBVOgJ1EtbTwu4NNEbktDsZ2ZKxyf5TepA",
+          appId: "1:616439791851:web:0e282eb7f0ea621ca8feae",
+          messagingSenderId: "616439791851",
+          projectId: "communal-967dd",
+          storageBucket: "communal-967dd.appspot.com"));
   runApp(const MyApp());
 }
 
