35c9a7ecc2a431023f91c964f7d054aca88354b4
Lans12345 <olanalans12345@gmail.com>
2023-10-13 12:43:17 +0800

    revised treepark and communal tabs

--- lib/screens/tabs/communal_tab.dart
+++ lib/screens/tabs/communal_tab.dart
@@ -1,103 +1,175 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:communal/utils/colors.dart';
+import 'package:communal/widgets/button_widget.dart';
 import 'package:communal/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
+
+import 'aboutus_tab.dart';
+import 'contactus_tab.dart';
 
 class CommunalTab extends StatelessWidget {
   const CommunalTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        height: 600,
-        width: 800,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
-          child: StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance
-                  .collection('Activities')
-                  .where('type', isEqualTo: 'Communal')
-                  .orderBy('dateTime', descending: true)
-                  .snapshots(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  print(snapshot.error);
-                  print('error');
-                  return const Center(child: Text('Error'));
-                }
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Padding(
-                    padding: EdgeInsets.only(top: 50),
-                    child: Center(
-                        child: CircularProgressIndicator(
-                      color: Colors.black,
-                    )),
-                  );
-                }
-
-                final data = snapshot.requireData;
-                return ListView.separated(
-                    itemBuilder: (context, index) {
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 150,
-                            width: 200,
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(10),
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                  data.docs[index]['img'],
-                                ),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 30,
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              TextWidget(
-                                text: data.docs[index]['name'],
-                                fontSize: 24,
-                                fontFamily: 'Bold',
-                                color: Colors.black,
-                              ),
-                              TextWidget(
-                                text: data.docs[index]['desc'],
-                                fontSize: 14,
-                                fontFamily: 'Regular',
-                                color: Colors.grey,
-                              ),
-                              TextWidget(
-                                text:
-                                    'on ${data.docs[index]['date']} at ${data.docs[index]['time']}',
-                                fontSize: 12,
-                                fontFamily: 'Regular',
-                                color: Colors.black,
-                              ),
-                            ],
-                          ),
-                        ],
-                      );
-                    },
-                    separatorBuilder: (context, index) {
-                      return const Divider();
-                    },
-                    itemCount: data.docs.length);
-              }),
-        ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 500,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/impasugong ranch.jpg'),
+                  fit: BoxFit.cover),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          TextWidget(
+            text: 'List of Services',
+            fontSize: 18,
+            fontFamily: 'Bold',
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 500,
+                    height: 250,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image:
+                              AssetImage('assets/images/impasugong ranch.jpg'),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextWidget(
+                    text: 'PRENUP',
+                    fontSize: 14,
+                    color: Colors.grey,
+                    fontFamily: 'Regular',
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextWidget(
+                    text: '2000 PHP',
+                    fontSize: 24,
+                    color: Colors.black,
+                    fontFamily: 'Bold',
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ButtonWidget(
+                    color: primary,
+                    radius: 100,
+                    label: 'Book Now',
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 500,
+                    height: 250,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image:
+                              AssetImage('assets/images/impasugong ranch.jpg'),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextWidget(
+                    text: 'FARM HOUSE',
+                    fontSize: 14,
+                    color: Colors.grey,
+                    fontFamily: 'Regular',
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextWidget(
+                    text: '1000 PHP/night',
+                    fontSize: 24,
+                    color: Colors.black,
+                    fontFamily: 'Bold',
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ButtonWidget(
+                    color: primary,
+                    radius: 100,
+                    label: 'Book Now',
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 20,
+          ),
+          TextWidget(
+            text: 'About Us',
+            fontSize: 32,
+            color: Colors.black,
+            fontFamily: 'Bold',
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(),
+          const AboutUsTab(),
+          const SizedBox(
+            height: 20,
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 20,
+          ),
+          TextWidget(
+            text: 'Contact Us',
+            fontSize: 32,
+            color: Colors.black,
+            fontFamily: 'Bold',
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(),
+          ContactUs(),
+        ],
       ),
     );
   }
